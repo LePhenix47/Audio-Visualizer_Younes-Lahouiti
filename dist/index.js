cc0d9cf97c1bf68f5ea2e8bd21b13697e6cc4e9d
Younes Lahouiti <loller763@gmail.com>
2023-04-21 13:35:54 +0200

    Addition of the config for the SASS files (vendor prefixers added + minified the CSS)

--- dist/index.js
+++ dist/index.js
@@ -1,96 +1,26 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
 
-/***/ "./src/index.ts":
-/*!**********************!*\
-  !*** ./src/index.ts ***!
-  \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+;// CONCATENATED MODULE: ./src/utils/functions/console.functions.ts
+/**
+ * The console methods are exported as separate methods through destructuring
+ */
+const { log, error, table, time, timeEnd, timeStamp, timeLog, assert, clear, count, countReset, group, groupCollapsed, groupEnd, trace, profile, profileEnd, warn, debug, info, dir, dirxml, } = console;
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _utils_functions_console_functions__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/functions/console.functions */ \"./src/utils/functions/console.functions.ts\");\n\n(0,_utils_functions_console_functions__WEBPACK_IMPORTED_MODULE_0__.log)(\"Hello world!\");\n\n\n//# sourceURL=webpack://22.-audiovisualizer/./src/index.ts?");
+;// CONCATENATED MODULE: ./src/index.ts
 
-/***/ }),
+log("Hello world!");
 
-/***/ "./src/utils/functions/console.functions.ts":
-/*!**************************************************!*\
-  !*** ./src/utils/functions/console.functions.ts ***!
-  \**************************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+})();
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"assert\": () => (/* binding */ assert),\n/* harmony export */   \"clear\": () => (/* binding */ clear),\n/* harmony export */   \"count\": () => (/* binding */ count),\n/* harmony export */   \"countReset\": () => (/* binding */ countReset),\n/* harmony export */   \"debug\": () => (/* binding */ debug),\n/* harmony export */   \"dir\": () => (/* binding */ dir),\n/* harmony export */   \"dirxml\": () => (/* binding */ dirxml),\n/* harmony export */   \"error\": () => (/* binding */ error),\n/* harmony export */   \"group\": () => (/* binding */ group),\n/* harmony export */   \"groupCollapsed\": () => (/* binding */ groupCollapsed),\n/* harmony export */   \"groupEnd\": () => (/* binding */ groupEnd),\n/* harmony export */   \"info\": () => (/* binding */ info),\n/* harmony export */   \"log\": () => (/* binding */ log),\n/* harmony export */   \"profile\": () => (/* binding */ profile),\n/* harmony export */   \"profileEnd\": () => (/* binding */ profileEnd),\n/* harmony export */   \"table\": () => (/* binding */ table),\n/* harmony export */   \"time\": () => (/* binding */ time),\n/* harmony export */   \"timeEnd\": () => (/* binding */ timeEnd),\n/* harmony export */   \"timeLog\": () => (/* binding */ timeLog),\n/* harmony export */   \"timeStamp\": () => (/* binding */ timeStamp),\n/* harmony export */   \"trace\": () => (/* binding */ trace),\n/* harmony export */   \"warn\": () => (/* binding */ warn)\n/* harmony export */ });\n/**\n * The console methods are exported as separate methods through destructuring\n */\nconst { log, error, table, time, timeEnd, timeStamp, timeLog, assert, clear, count, countReset, group, groupCollapsed, groupEnd, trace, profile, profileEnd, warn, debug, info, dir, dirxml, } = console;\n\n\n//# sourceURL=webpack://22.-audiovisualizer/./src/utils/functions/console.functions.ts?");
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+// extracted by mini-css-extract-plugin
 
-/***/ })
+})();
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.ts");
-/******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
   entry: {
-    index: "./src/index.ts",
+    index: ["./src/index.ts", "./src/style/sass/main.scss"],
   },
-  mode: "development",
+  mode: "production",
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "./dist"),
-    // Cleans the output directory before each build
-    //to avoid having the same files
     clean: true,
+    //The following line generates a single CSS file
+    // instead of a separate JS file for the CSS
+    assetModuleFilename: "css/style[ext]",
   },
   module: {
     rules: [
@@ -19,7 +23,40 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer({
+                    grid: true,
+                  }),
+                ],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
     ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      ignoreOrder: true,
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
   },
   resolve: {
     extensions: [".ts", ".js"],
